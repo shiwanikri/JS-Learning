e5dba4a47faa32ec0c0c0c6621d148514dfb7e56
shiwanikri <65379383+shiwanikri@users.noreply.github.com>
2024-03-17 15:04:41 +0000

    coner

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,5 +25,11 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// console.log(1 + "2"); //12
+// console.log("1" + 2);  //12
+// console.log("1" + 2 + 2);  //122
+// console.log(1 + 2 + "2");  //32
+
