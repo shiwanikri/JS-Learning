1373e25b0f0d3a6c73de909e47a1b27567bcba02
shiwanikri <65379383+shiwanikri@users.noreply.github.com>
2024-03-18 17:39:07 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,35 @@
+const score = 100
+// console.log(score);
+
+const balance  = new Number(400)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const anotherNumber  = 123.8946
+// console.log(anotherNumber.toPrecision(4));
+
+const hundred  = 1000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+
+// +++++++++++++++++++++++++++++++   Maths  ++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.4));
+// console.log(Math.min(5,3,6,7));
+// console.log(Math.max(6,3,5,2));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor((Math.random() * 10) + 1));
+
+const min = 10 
+const max = 20
+console.log(Math.floor((Math.random() * (max - min + 1)) + min));
+
