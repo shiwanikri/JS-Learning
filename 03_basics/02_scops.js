c315ce49f5ca3e01dd09ce2fd1eedc908a85f096
shiwanikri <65379383+shiwanikri@users.noreply.github.com>
2024-03-22 12:44:59 +0000

    scope

---
+++ 03_basics/02_scops.js
@@ -0,0 +1,23 @@
+
+if(true){
+    let a = 10
+const b = 20
+var c = 30
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+console.log(addOne(4));
+function addOne(num){
+    return num + 1
+}
+
+addTwo(6)
+const addTwo = function(num){
+    return num + 3
+}
+addTwo(6)
+
